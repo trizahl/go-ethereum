ccd19937dc04e9a6eeb13c5d585df7dd0c907ec2
trizahl <121090956+trizahl@users.noreply.github.com>
2025-02-14 16:37:22 +0100

    Update goimports.sh

--- build/goimports.sh
+++ build/goimports.sh
@@ -12,6 +12,36 @@ find_files() {
     \) -name '*.go'
 }
 
+find_files() {
+  find . ! \( \
+      \( \
+        -path '.github' \
+        -o -path './build/_workspace' \
+        -o -path './build/bin' \
+        -o -path './crypto/bn256' \
+        -o -path '*/vendor/*' \
+      \) -prune \
+    \) -name '*.go'
+}
+
+GOFMT="gofmt -s -w"
+GOIMPORTS="goimports -w"
+find_files | xargs $GOFMT
+find_files() {
+  find . ! \( \
+      \( \
+        -path '.github' \
+        -o -path './build/_workspace' \
+        -o -path './build/bin' \
+        -o -path './crypto/bn256' \
+        -o -path '*/vendor/*' \
+      \) -prune \
+    \) -name '*.go'
+}
+
+GOFMT="gofmt -s -w"
+GOIMPORTS="goimports -w"
+find_files | xargs $GOFMT
 GOFMT="gofmt -s -w"
 GOIMPORTS="goimports -w"
 find_files | xargs $GOFMT
